42eed91636551c4c71017cbe0fecc3cf481e94b7
ian mulvany <ian@mulvany.net>
2013-04-23 14:53:03 +0100

    updated metadtaa

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'elife-chef-dev-template'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
+maintainer       'eLife'
+maintainer_email 'i.mulvany@elifesciences.org'
+license          'MIT'
 description      'Installs/Configures elife-chef-dev-template'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
