b60393d1a06c9527d9fd1957a51653a463aa17dd
Graham Nott <gnott@starglobal.ca>
2013-05-01 09:55:32 -0700

    Separated and improved localgit.

---
+++ recipes/localgit.rb
@@ -0,0 +1,40 @@
+#
+# Cookbook Name:: elife-chef-dev-template
+# Recipe:: localgit
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "git"
+include_recipe "elife-chef-dev-template::default"
+
+# where to put our repositories
+localgit = "/home/vagrant/localgit"
+
+directory localgit do
+  owner "root"
+  group "root"
+  mode 0755
+  action :create
+end
+
+# install git repos to our local path
+git_repos = Array.new
+git_repos.push ["git://github.com/elifesciences/", "elife-chef-dev-template"]
+git_repos.push ["git://github.com/elifesciences/", "elife-api-prototype"]
+git_repos.push ["git://github.com/elifesciences/", "elife-bot"]
+git_repos.push ["git://github.com/elifesciences/", "elife-articles"]
+
+git_repos.each do |repos|
+  base_uri = repos[0]
+  repos_name = repos[1] 
+  repos_uri = base_uri + repos_name + ".git"
+  repos_dir = localgit + "/" + repos_name
+  git repos_dir do
+    repository repos_uri
+    reference "master"
+    action :sync
+  end
+end
